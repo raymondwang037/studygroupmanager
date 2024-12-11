310429a5d0a6a0c017f297a6510151ecd2817eaf
raymondwang037 <raymondwang037@gmail.com>
2024-12-11 11:38:31 -0500

    t

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,8 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
 
+
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
