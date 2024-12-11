4d096a35f757c64efa9057da0dc009619b0e90e3
raymondwang037 <raymondwang037@gmail.com>
2024-12-11 10:56:24 -0500

    please

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   'http://localhost:5173', // Local frontend
-  'https://studygroupmanager.vercel.app', // Vercel frontend
+  'https://studygroupmanager-1.onrender.com', // Vercel frontend
 ];
 
 app.use(
