8c5e9de3461b9cfd18d22978cd8b52f0bf9bbc99
raymondwang037 <raymondwang037@gmail.com>
2024-12-11 12:40:42 -0500

    htht

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // Add password field
-  userType: { type: String, enum: ['admin', 'student'], default: 'student' }, // New field
+  password: { type: String, required: true },
+  userType: { type: String, enum: ['admin', 'student'], default: 'student' },
 }, { optimisticConcurrency: true });
 
 module.exports = mongoose.model('Student', studentSchema);
